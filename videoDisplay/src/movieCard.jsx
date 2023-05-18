216449d968278b43b1f87ebb35fe4ce6620eedc2
Gatete Ishema Angelo Chrristian <ishgatetechristian@gmail.com>
2023-05-18 12:03:48 +0300

    Adding updates on single page

--- videoDisplay/src/movieCard.jsx
+++ videoDisplay/src/movieCard.jsx
@@ -1,32 +1,37 @@
 import "./All.css";
 import Icon from "./Icon.jsx";
 import Bookmark from "./assets/bookmark-black.svg";
+import { Link } from "react-router-dom";
 
 const MovieCard = (props) => {
   return (
     <>
-      <div
-        style={{ backgroundImage: `url(${props.backImage})` }}
-        className={props.movieClass + " default "}
-      >
-        <div className="feedLogo">
-          <Icon IconClass="sideIcon" IconPic={Bookmark} />
-        </div>
-      </div>
-      <div className={" cardContent " + props.cardContentClass}>
-        <div className="movieDetails">
-          <div>
-            <h4>{props.year}</h4>
+      <div className="movieCard">
+        <Link to="/singleCard">
+          <div
+            style={{ backgroundImage: `url(${props.backImage})` }}
+            className={props.movieClass + " default "}
+          >
+            <div className="feedLogo">
+              <Icon IconClass="sideIcon" IconPic={Bookmark} />
+            </div>
+          </div>
+        </Link>
+        <div className={" cardContent " + props.cardContentClass}>
+          <div className="movieDetails">
+            <div>
+              <h4>{props.year}</h4>
+            </div>
+            <div>
+              <h4>{props.category}</h4>
+            </div>
+            <div>
+              <h4>{props.pg}</h4>
+            </div>
           </div>
           <div>
-            <h4>{props.category}</h4>
+            <h3 className="movieName">{props.movieName}</h3>
           </div>
-          <div>
-            <h4>{props.pg}</h4>
-          </div>
-        </div>
-        <div>
-          <h3 className="movieName">{props.movieName}</h3>
         </div>
       </div>
     </>
--- videoDisplay/src/pages/Single.jsx
+++ videoDisplay/src/pages/Single.jsx
@@ -1,18 +1,87 @@
 // import React from "react";
 import "../All.css";
 import MainLayout from "../MainLayout";
+import MovieCard from "../movieCard.jsx";
+import Avenger from "../assets/Avengers.jpg";
+import Record from "../assets/record.mp4";
+import Record1 from "../assets/record1.mp4";
 
 export default function Single() {
   return (
     <>
-      <div className="singleContainer">
-        <MainLayout>
-          <div>Single</div>
-          <div>
-            <h3>From Single</h3>
+      <MainLayout>
+        <div className="mainContent">
+          <div className="movieHeading">
+            <div>
+              <h5>Movie: </h5>
+            </div>
+            <div>
+              <h4>Avengers</h4>
+            </div>
+            <div>
+              <p>:2021</p>
+            </div>
           </div>
-        </MainLayout>
-      </div>
+          {/* <div>
+            <MovieCard
+              backImage={Avenger}
+              movieClass="bigCard"
+              cardContentClass="moveLeft"
+              year="2019"
+              category="Movie"
+              pg="PG"
+              movieName="Beyong Earth"
+            />
+          </div> */}
+          <div className="video">
+            <video controls width="100%">
+              {" "}
+              <source src={Record1} type="video/mp4" />
+              <source src={Record1} type="video/webm" />
+              Sorry cant be displayed
+            </video>
+          </div>
+          <div className="description">
+            <div>
+              <h3>Description</h3>
+            </div>
+            <div>
+              <p width="200px">
+                After the events of the previous avengers, The universe is in
+                ruins. with help of remaining allies, the Avengers assemble
+                again in order to stop Thanos whose aim is to eradicate life as
+                we see it and restore the order of the universe. But will they
+                succeed
+              </p>
+            </div>
+            <div className="director">
+              <div>
+                <h4>Director: </h4>
+              </div>
+              <div>
+                <p>Antony and Joe Russo</p>
+              </div>
+            </div>
+            <div className="cast">
+              <div>
+                <h4>Cast: </h4>
+              </div>
+              <div>
+                <p>
+                  Scarlett Johanson, Robert Downey Jr, Chris Evans, Mark
+                  Rufallo, Jeremy Renner, Josh Brolin, Karen Gillan, Paul Rudd,
+                  Tom Holland, Tessa Thompson, Gywenth Paltwol
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h3>Browse More</h3>
+            </div>
+          </div>
+        </div>
+      </MainLayout>
     </>
   );
 }
